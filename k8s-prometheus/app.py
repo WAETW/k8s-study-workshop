44e77ae860579bfcb6f5ab5902e3656fac1ee637
WAETW <zxc8110924@gmail.com>
2019-11-17 02:48:57 +0800

    Started!!

---
+++ k8s-prometheus/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, Response
+import prometheus_client
+from prometheus_client import Counter
+
+app = Flask(__name__)
+
+request_counter = Counter('cc', 'A counter') 
+
+@app.route("/", methods=['GET'])
+def hello():
+    request_counter.inc()
+    return "Hello, World!"
+
+@app.route("/Metrics", methods=['GET'])
+def request_count():
+    return Response(prometheus_client.generate_latest(request_counter), mimetype='text/plain')
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True, host='0.0.0.0')
