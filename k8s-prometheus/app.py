2c2ad2b22e172dd6b0cc9c6a888d36b5a856f600
WAETW <zxc8110924@gmail.com>
2019-12-20 16:00:17 +0800

    add login signup function to mongodb 😃

--- k8s-prometheus/app.py
+++ k8s-prometheus/app.py
@@ -2,18 +2,26 @@ from flask import Flask, Response, render_template, request
 import prometheus_client
 from prometheus_client import Counter
 import hashlib
+import json
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-
+"""
 app.config.update(
     MONGO_HOST='localhost',
     MONGO_PORT=27017,
     MONGO_USERNAME='admin',
     MONGO_PASSWORD='admin',
     MONGO_DBNAME='user'
-)
+)"""
+app.config["MONGO_URI"] = "mongodb://172.17.0.2:27017/user"
+mongo = PyMongo(app)
 request_counter = Counter('cc', 'A counter') 
+
+def password_hash(password):
+        sha = hashlib.sha256()
+        sha.update(password.encode("utf-8"))
+        return sha.hexdigest()
 
 @app.route("/", methods=['GET'])
 def index():
@@ -24,14 +32,24 @@ def index():
 def login():
     username = request.values['username']
     password = request.values['password']
-    def password_hash(password):
-        sha = hashlib.sha256()
-        sha.update(password.encode("utf-8"))
-        return sha.hexdigest()
     has_hash = password_hash(password)
-    return "Hello " + username + " Hashed password: " + has_hash
+    user = mongo.db.users.find_one({'username':username, 'password':has_hash})
+    if user != None:
+        return "Hello " + username
+    else:
+        return "No user find!!!"
 
-
+@app.route("/signup", methods=['POST'])
+def signup():
+    username = request.values['username']
+    password = request.values['password']
+    has_hash = password_hash(password)
+    user = {
+        'username':username,
+        'password':has_hash,
+    }
+    mongo.db.users.insert_one(user)
+    return "Thank for signing up! Username: " + username 
 @app.route("/metrics", methods=['GET'])
 def request_count():
     return Response(prometheus_client.generate_latest(request_counter), mimetype='text/plain')
